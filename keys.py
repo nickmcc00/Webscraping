8ff9c934aea7961ace773f05d024ac0b31b1263f
nickmcc00 <nicholas_mccullough1@baylor.edu>
2023-04-24 15:38:19 -0500

    worked

---
+++ keys.py
@@ -0,0 +1,18 @@
+consumer_key = ''
+consumer_secret = ''
+access_token = ''
+access_token_secret = ''
+
+mapquest_key = ''
+
+
+api_key =  ''
+api_key_secret = ''
+bearer_token = ''
+
+
+#phone_number = '+1 507 516 7290'
+
+accountSID = 'AC58f94651d591f4acf129f74bf128ac6c'
+
+authToken = '17ed2190d30be359bf7b943a1537ccf3'
--- webscraping-Bible.py
+++ webscraping-Bible.py
@@ -3,11 +3,54 @@ from urllib.request import urlopen
 from bs4 import BeautifulSoup
 from urllib.request import urlopen, Request
 
+chapters = list(range(1,22))
+
+random_chapter = random.choice(chapters)
+
+if random_chapter < 10:
+    random_chapter = '0' + str(random_chapter)
+else:
+    random_chapter = str(random_chapter)
 
 
-webpage = 'https://ebible.org/asv/JHN'
+url = 'https://ebible.org/asv/JHN' + '04' + '.htm'
+#url = 'https://ebible.org/asv/JHN' + random_chapter + '.htm'
+
 
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.3'}
-req = Request(webpage, headers=headers)
+req = Request(url, headers=headers)
 
-num = random.randint(1, 21)
+webpage = urlopen(req).read()
+
+soup = BeautifulSoup(webpage, 'html.parser')
+
+print(soup.title.text)
+
+
+page_verses = soup.findAll('div', class_='main')
+
+for verses in page_verses:
+    verse_list = verses.text.split(".")
+
+mychoice = random.choice(verse_list[:-5])
+
+verse = f'Chapter: {random_chapter} Verse: {mychoice}'
+
+print(verse)
+
+
+import keys
+from twilio.rest import Client
+
+client = Client(keys.accountSID, keys.authToken)
+
+TwilioNumber = "+15075167290"
+
+mycellphone = "+18322312653"
+
+textmessage = client.messages.create(to=mycellphone, from_=TwilioNumber, body=verse)
+
+
+
+
+

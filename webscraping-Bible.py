9e92d56f3d6a19cbd81abef6f93d254933229264
nickmcc00 <nicholas_mccullough1@baylor.edu>
2023-04-24 15:06:23 -0500

    created

--- webscraping-Bible.py
+++ webscraping-Bible.py
@@ -10,3 +10,4 @@ webpage = 'https://ebible.org/asv/JHN'
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.3'}
 req = Request(webpage, headers=headers)
 
+num = random.randint(1, 21)

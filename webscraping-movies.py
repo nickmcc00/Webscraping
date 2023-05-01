e9aa5e42d007f2161a1e83ce26c60164a6b0c67e
nickmcc00 <nicholas_mccullough1@baylor.edu>
2023-05-01 15:47:34 -0500

    worked

--- webscraping-movies.py
+++ webscraping-movies.py
@@ -19,6 +19,55 @@ title = soup.title
 
 print(title.text)
 
-table_rows = title.findAll("tr")
-print(table_rows)
+
+
+wb = xl.Workbook()
+
+ws = wb.active
+
+ws.title = 'Box Office Report'
+
+ws['A1'] = 'No.'
+ws['B1'] = 'Movie Title'
+ws['C1'] = 'Release Date'
+ws['D1'] = 'Gross'
+ws['E1'] = 'Total Gross'
+ws['F1'] = '% of Total Gross'
+
+movie_rows = soup.findAll('tr')
+
+#webscraping
+for x in range(1, 6):
+    td = movie_rows[x].findAll('td')
+    no = td[0].text
+    title = td[1].text
+    gross = int(td[5].text.replace(",", "").replace("$", ""))
+    total_gross = int(td[7].text.replace(",", "").replace("$", ""))
+    release_date = td[8].text
+
+    percent_gross = round((gross / total_gross) * 100,2)
+
+#writing to document
+    ws['A' + str(x+1)] = no
+    ws['B' + str(x+1)] = title
+    ws['C' + str(x+1)] = release_date
+    ws['D' + str(x+1)] = gross
+    ws['E' + str(x+1)] = total_gross
+    ws['F' + str(x+1)] = str(percent_gross) + '%'
+
+
+ws.column_dimensions['A'].width = 5
+
+
+
+header_font = Font(size=16, bold=True)
+
+for cell in ws[1:1]:
+    cell.font = header_font
+
+
+wb.save("BoxOfficeReport.xlsx")
+
+    
+
 
